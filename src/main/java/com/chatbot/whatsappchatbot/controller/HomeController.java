2999c70a9e3e73dd6f5b9c9949ef2cd2e3bd691b
PoornaSankar <kpoornasankar@gmail.com>
2026-04-23 21:47:08 +0530

    fix package mismatch

--- src/main/java/com/chatbot/whatsappchatbot/controller/HomeController.java
+++ src/main/java/com/chatbot/whatsappchatbot/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.chatbot.whatsappchatbot;
+package com.chatbot.whatsappchatbot.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
